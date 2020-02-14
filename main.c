513f5db3a9d6da9505f6338d0f19d39b9da6d5b2
RatchananSakTNI <58293937+RatchananSakTNI@users.noreply.github.com>
2020-02-15 02:50:19 +0700

    Upgrade HC-SR04 code to measure at full-range.

--- main.c
+++ main.c
@@ -8,27 +8,19 @@
 #include "stm32l1xx_ll_utils.h"
 #include "stm32l1xx_ll_system.h"
 #include "stm32l1xx_ll_tim.h"
+#include "dwt_delay.h"
 
-//#include "dwt_delay.h"
-
-/*already implemented */
 void SystemClock_Config(void);
 void GPIO_Config(void);
 void TIMx_IC_Config(void);
-void TIMBase_Config(void);
+void TIM_TRIG_Config(void);
 void TIM4_IRQHandler(void);
 void TIM2_IRQHandler(void);
 void SevenSeg(uint8_t*);
 
-uint32_t TIM4CLK;
-uint32_t PSC;
-uint32_t IC1PSC;
-uint16_t uwIC3 = 0;
-uint16_t uwIC4 = 0;
-uint16_t uwDiff = 0;
-float period = 0;
 float distance;
-uint16_t distance_cm;
+uint16_t to_display;
+uint32_t micro_time = 0;
 
 uint8_t seg[4];
 uint32_t digit[4] = { LL_GPIO_PIN_0, LL_GPIO_PIN_1, LL_GPIO_PIN_2, LL_GPIO_PIN_3 };
@@ -36,19 +28,18 @@ uint32_t digit[4] = { LL_GPIO_PIN_0, LL_GPIO_PIN_1, LL_GPIO_PIN_2, LL_GPIO_PIN_3
 int main()
 {
 	SystemClock_Config();
-	//DWT_Init();
+	DWT_Init();
 	GPIO_Config();
 	TIMx_IC_Config();
-	TIMBase_Config();
+	TIM_TRIG_Config();
 
 	while (1)
 	{	
 		// saparate digit example
-		
-		seg[0] = distance_cm/1000;
-		seg[1] = (distance_cm/100)%10;
-		seg[2] = (distance_cm%100)/10;
-		seg[3] = distance_cm%10;
+		seg[0] = to_display/1000;
+		seg[1] = (to_display/100)%10;
+		seg[2] = (to_display%100)/10;
+		seg[3] = to_display%10;
 		
 		SevenSeg(seg);
 	}
@@ -64,9 +55,6 @@ void GPIO_Config()
 	gpio_conf.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
 	gpio_conf.Pull = LL_GPIO_PULL_NO;
 	gpio_conf.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
-	
-	gpio_conf.Pin = LL_GPIO_PIN_8;
-	LL_GPIO_Init(GPIOA, &gpio_conf);
 	
 	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
 
@@ -99,6 +87,11 @@ void GPIO_Config()
 	LL_GPIO_Init(GPIOC, &gpio_conf);
 	
 	gpio_conf.Mode = LL_GPIO_MODE_ALTERNATE;
+	
+	gpio_conf.Alternate = LL_GPIO_AF_1;
+	gpio_conf.Pin = LL_GPIO_PIN_15;
+	LL_GPIO_Init(GPIOA, &gpio_conf);
+	
 	gpio_conf.Alternate = LL_GPIO_AF_2;
 	gpio_conf.Pin = LL_GPIO_PIN_8;
 	LL_GPIO_Init(GPIOB, &gpio_conf);
@@ -133,68 +126,60 @@ void TIMx_IC_Config(void)
 	LL_TIM_EnableCounter(TIM4);
 }
 
-// for 1 ms trigger
-void TIMBase_Config()
+// for trigger (30ms base)
+void TIM_TRIG_Config()
 {
 	LL_TIM_InitTypeDef tim_init;
 	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM2);
 	
 	tim_init.ClockDivision = LL_TIM_CLOCKDIVISION_DIV1;
 	tim_init.CounterMode = LL_TIM_COUNTERMODE_UP;
-	tim_init.Autoreload = 80 - 1; // 8 for 1ms , 80 for 10ms
-	tim_init.Prescaler = 4000 - 1;
+	tim_init.Autoreload = 3000 - 1;
+	tim_init.Prescaler = 320 - 1;
 	
 	LL_TIM_Init(TIM2, &tim_init);
 	
-	LL_TIM_EnableIT_UPDATE(TIM2);
+	// TIM_OC
+	LL_TIM_OC_InitTypeDef tim_oc_initstructure;
 	
+	tim_oc_initstructure.OCState = LL_TIM_OCSTATE_DISABLE;
+	tim_oc_initstructure.OCMode = LL_TIM_OCMODE_PWM1;
+	tim_oc_initstructure.OCPolarity = LL_TIM_OCPOLARITY_HIGH;
+	tim_oc_initstructure.CompareValue = 2; // 20us pulse
+	LL_TIM_OC_Init(TIM2, LL_TIM_CHANNEL_CH1, &tim_oc_initstructure);
+	/*Interrupt Configure*/
 	NVIC_SetPriority(TIM2_IRQn, 0);
 	NVIC_EnableIRQ(TIM2_IRQn);
+	LL_TIM_EnableIT_CC1(TIM2);
 	
+	/*Start Output Compare in PWM Mode*/
+	LL_TIM_CC_EnableChannel(TIM2, LL_TIM_CHANNEL_CH1);
 	LL_TIM_EnableCounter(TIM2);
 }
 
 void TIM4_IRQHandler(void)
 {
-	//Detect 1st rising edge
 	if (LL_TIM_IsActiveFlag_CC3(TIM4) == SET)
 	{
 		LL_TIM_ClearFlag_CC3(TIM4);
-		uwIC3 = LL_TIM_IC_GetCaptureCH3(TIM4);
-	}
-
-	//Detect after falling edge
-	if (LL_TIM_IsActiveFlag_CC4(TIM4) == SET)
-	{
+		micro_time = 0;
+		while(LL_TIM_IsActiveFlag_CC4(TIM4) != SET)
+		{
+			micro_time++;
+			DWT_Delay(1);
+		}
 		LL_TIM_ClearFlag_CC4(TIM4);
-		uwIC4 = LL_TIM_IC_GetCaptureCH4(TIM4);
-
-		if (uwIC4 > uwIC3)
-			uwDiff = uwIC4 - uwIC3;
-		else if (uwIC4 < uwIC3)
-			uwDiff = ((LL_TIM_GetAutoReload(TIM4) - uwIC3) + uwIC4) + 1;
-
-		//Period calculation
-		PSC = LL_TIM_GetPrescaler(TIM4) + 1;
-		TIM4CLK = SystemCoreClock / PSC;
-		IC1PSC = __LL_TIM_GET_ICPSC_RATIO(LL_TIM_IC_GetPrescaler(TIM4, LL_TIM_CHANNEL_CH3));
-
-		period = (uwDiff*(PSC) * 1.0) / (TIM4CLK *IC1PSC * 1.0); //calculate uptime period
-
-		// calculate distance
-		distance = (period * 340) / 2.0;
-		distance_cm = distance * 100 *100; // for display
-	}
+		micro_time = micro_time * 2;
+		distance = micro_time * 340.0 / 2.0 / 1000000.0 * 1.40;
+		to_display = distance * 100;
+		}
 }
 
-// for 1 ms trigger
 void TIM2_IRQHandler()
 {
-	if(LL_TIM_IsActiveFlag_UPDATE(TIM2) == SET)
+	if(LL_TIM_IsActiveFlag_CC1(TIM2) == SET)
 	{
-		LL_TIM_ClearFlag_UPDATE(TIM2);
-		
-		LL_GPIO_TogglePin(GPIOA, LL_GPIO_PIN_8);
+		LL_TIM_ClearFlag_CC1(TIM2);
 	}
 }
 
