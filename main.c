5d744c018ca2388294bf350819e30f61a519666b
RatchananSakTNI <58293937+RatchananSakTNI@users.noreply.github.com>
2020-02-17 01:24:58 +0700

    Add motor part and detection algorithm.

--- main.c
+++ main.c
@@ -9,18 +9,29 @@
 #include "stm32l1xx_ll_system.h"
 #include "stm32l1xx_ll_tim.h"
 #include "dwt_delay.h"
+#include "math.h"
 
 void SystemClock_Config(void);
 void GPIO_Config(void);
 void TIMx_IC_Config(void);
 void TIM_TRIG_Config(void);
+void TIM_Motor_Config(void);
 void TIM4_IRQHandler(void);
 void TIM2_IRQHandler(void);
+void TIM3_IRQHandler(void);
 void SevenSeg(uint8_t*);
 
 float distance;
 uint16_t to_display;
 uint32_t micro_time = 0;
+uint16_t r = 0;
+uint16_t indx = 0;
+uint32_t count = 0;
+uint8_t state = 0;
+uint8_t flag = 0;
+float ori[60];
+float first;
+float later;
 
 uint8_t seg[4];
 uint32_t digit[4] = { LL_GPIO_PIN_0, LL_GPIO_PIN_1, LL_GPIO_PIN_2, LL_GPIO_PIN_3 };
@@ -32,15 +43,20 @@ int main()
 	GPIO_Config();
 	TIMx_IC_Config();
 	TIM_TRIG_Config();
+	TIM_Motor_Config();
+	
+	LL_GPIO_SetOutputPin(GPIOB, LL_GPIO_PIN_7);
+	LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_4, LL_GPIO_MODE_OUTPUT);
+	LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_4);
 
 	while (1)
-	{	
+	{
 		// saparate digit example
-		seg[0] = to_display/1000;
-		seg[1] = (to_display/100)%10;
-		seg[2] = (to_display%100)/10;
-		seg[3] = to_display%10;
-		
+		seg[0] = to_display / 1000;
+		seg[1] = (to_display / 100) % 10;
+		seg[2] = (to_display % 100) / 10;
+		seg[3] = to_display % 10;
+
 		SevenSeg(seg);
 	}
 }
@@ -50,12 +66,14 @@ void GPIO_Config()
 	LL_GPIO_InitTypeDef gpio_conf;
 
 	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
-	
+
 	gpio_conf.Mode = LL_GPIO_MODE_OUTPUT;
 	gpio_conf.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
 	gpio_conf.Pull = LL_GPIO_PULL_NO;
 	gpio_conf.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
-	
+	gpio_conf.Pin = LL_GPIO_PIN_2;
+	LL_GPIO_Init(GPIOA, &gpio_conf);
+
 	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
 
 	gpio_conf.Pin = LL_GPIO_PIN_2;
@@ -74,6 +92,8 @@ void GPIO_Config()
 	LL_GPIO_Init(GPIOB, &gpio_conf);
 	gpio_conf.Pin = LL_GPIO_PIN_3;
 	LL_GPIO_Init(GPIOB, &gpio_conf);
+	gpio_conf.Pin = LL_GPIO_PIN_7;
+	LL_GPIO_Init(GPIOB, &gpio_conf);
 
 	LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOC);
 
@@ -85,17 +105,22 @@ void GPIO_Config()
 	LL_GPIO_Init(GPIOC, &gpio_conf);
 	gpio_conf.Pin = LL_GPIO_PIN_3;
 	LL_GPIO_Init(GPIOC, &gpio_conf);
-	
+
 	gpio_conf.Mode = LL_GPIO_MODE_ALTERNATE;
-	
+
 	gpio_conf.Alternate = LL_GPIO_AF_1;
 	gpio_conf.Pin = LL_GPIO_PIN_15;
 	LL_GPIO_Init(GPIOA, &gpio_conf);
-	
+
 	gpio_conf.Alternate = LL_GPIO_AF_2;
 	gpio_conf.Pin = LL_GPIO_PIN_8;
 	LL_GPIO_Init(GPIOB, &gpio_conf);
 	gpio_conf.Pin = LL_GPIO_PIN_9;
+	LL_GPIO_Init(GPIOB, &gpio_conf);
+
+	gpio_conf.Pin = LL_GPIO_PIN_5;
+	LL_GPIO_Init(GPIOB, &gpio_conf);
+	gpio_conf.Pin = LL_GPIO_PIN_4;
 	LL_GPIO_Init(GPIOB, &gpio_conf);
 }
 
@@ -131,17 +156,17 @@ void TIM_TRIG_Config()
 {
 	LL_TIM_InitTypeDef tim_init;
 	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM2);
-	
+
 	tim_init.ClockDivision = LL_TIM_CLOCKDIVISION_DIV1;
 	tim_init.CounterMode = LL_TIM_COUNTERMODE_UP;
 	tim_init.Autoreload = 3000 - 1;
 	tim_init.Prescaler = 320 - 1;
-	
+
 	LL_TIM_Init(TIM2, &tim_init);
-	
+
 	// TIM_OC
 	LL_TIM_OC_InitTypeDef tim_oc_initstructure;
-	
+
 	tim_oc_initstructure.OCState = LL_TIM_OCSTATE_DISABLE;
 	tim_oc_initstructure.OCMode = LL_TIM_OCMODE_PWM1;
 	tim_oc_initstructure.OCPolarity = LL_TIM_OCPOLARITY_HIGH;
@@ -151,19 +176,52 @@ void TIM_TRIG_Config()
 	NVIC_SetPriority(TIM2_IRQn, 0);
 	NVIC_EnableIRQ(TIM2_IRQn);
 	LL_TIM_EnableIT_CC1(TIM2);
-	
+
 	/*Start Output Compare in PWM Mode*/
 	LL_TIM_CC_EnableChannel(TIM2, LL_TIM_CHANNEL_CH1);
 	LL_TIM_EnableCounter(TIM2);
 }
 
-void TIM4_IRQHandler(void)
+void TIM_Motor_Config()
+{
+	LL_TIM_InitTypeDef timbase_initstructure;
+
+	LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM3);
+
+	timbase_initstructure.ClockDivision = LL_TIM_CLOCKDIVISION_DIV2;
+	timbase_initstructure.CounterMode = LL_TIM_COUNTERMODE_UP;
+	timbase_initstructure.Autoreload = 20 - 1;
+	timbase_initstructure.Prescaler = 16000 - 1;
+
+	LL_TIM_Init(TIM3, &timbase_initstructure);
+
+	LL_TIM_OC_InitTypeDef tim_oc_initstructure;
+
+	tim_oc_initstructure.OCState = LL_TIM_OCSTATE_DISABLE;
+	tim_oc_initstructure.OCMode = LL_TIM_OCMODE_PWM1;
+	tim_oc_initstructure.OCPolarity = LL_TIM_OCPOLARITY_HIGH;
+	tim_oc_initstructure.CompareValue = LL_TIM_GetAutoReload(TIM3)*0.7; //100% duty
+	LL_TIM_OC_Init(TIM3, LL_TIM_CHANNEL_CH1, &tim_oc_initstructure);
+	LL_TIM_OC_Init(TIM3, LL_TIM_CHANNEL_CH2, &tim_oc_initstructure);
+	/*Interrupt Configure*/
+	NVIC_SetPriority(TIM3_IRQn, 0);
+	NVIC_EnableIRQ(TIM3_IRQn);
+	LL_TIM_EnableIT_CC1(TIM3);
+	LL_TIM_EnableIT_CC2(TIM3);
+
+	/*Start Output Compare in PWM Mode*/
+	LL_TIM_CC_EnableChannel(TIM3, LL_TIM_CHANNEL_CH1);
+	LL_TIM_CC_EnableChannel(TIM3, LL_TIM_CHANNEL_CH2);
+	LL_TIM_EnableCounter(TIM3);
+}
+
+void TIM4_IRQHandler()
 {
 	if (LL_TIM_IsActiveFlag_CC3(TIM4) == SET)
 	{
 		LL_TIM_ClearFlag_CC3(TIM4);
 		micro_time = 0;
-		while(LL_TIM_IsActiveFlag_CC4(TIM4) != SET)
+		while (LL_TIM_IsActiveFlag_CC4(TIM4) != SET)
 		{
 			micro_time++;
 			DWT_Delay(1);
@@ -172,14 +230,108 @@ void TIM4_IRQHandler(void)
 		micro_time = micro_time * 2;
 		distance = micro_time * 340.0 / 2.0 / 1000000.0 * 1.40;
 		to_display = distance * 100;
-		}
+	}
+//	if (LL_TIM_IsActiveFlag_CC4(TIM4) == SET)
+//	{
+//		LL_TIM_ClearFlag_CC4(TIM4);
+//	}
 }
 
+// trigger 30ms
 void TIM2_IRQHandler()
 {
-	if(LL_TIM_IsActiveFlag_CC1(TIM2) == SET)
+	if (LL_TIM_IsActiveFlag_UPDATE(TIM2) == SET)
+	{
+		LL_TIM_ClearFlag_UPDATE(TIM2);
+		
+		if(flag < 1)
+		{
+		}
+		else
+		{	
+			ori[indx] = distance;
+		}
+		indx++;
+	}
+	
+	if (LL_TIM_IsActiveFlag_CC1(TIM2) == SET)
 	{
 		LL_TIM_ClearFlag_CC1(TIM2);
+	}
+}
+
+void TIM3_IRQHandler(void)
+{
+	uint16_t i;
+	float temp = 0;
+	
+	if (LL_TIM_IsActiveFlag_UPDATE(TIM3) == SET)
+	{
+		LL_TIM_ClearFlag_UPDATE(TIM3);
+		count++;
+
+		if (count >= 150 + 1) // 50 = 1s
+		{
+			if(flag < 1)
+			{
+				flag = 1;
+			}
+			else if (flag == 1)
+			{
+				for(i=0;i<indx;i++)
+				{
+					temp += ori[i];
+				}
+				first = temp/indx;
+				temp = 0;
+				flag = 2;
+			}
+			else if (flag == 2)
+			{
+				temp = 0;
+				flag = 3;
+			}
+			else if (flag == 3)
+			{
+				for(i=0;i<indx;i++)
+				{
+					temp += ori[i];
+				}
+				later = temp/indx;
+				temp = 0;
+				flag = 2;
+				
+				if (fabs(first - later) >= (first*0.2))
+				{
+					LL_GPIO_SetOutputPin(GPIOA, LL_GPIO_PIN_2);
+				}
+			}
+			indx = 0;
+			
+			if (state <= 0)
+			{
+				LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_5, LL_GPIO_MODE_OUTPUT);
+				LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_5);
+				LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_4, LL_GPIO_MODE_ALTERNATE);
+				state = 1;
+			}
+			else
+			{
+				LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_4, LL_GPIO_MODE_OUTPUT);
+				LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_4);
+				LL_GPIO_SetPinMode(GPIOB, LL_GPIO_PIN_5, LL_GPIO_MODE_ALTERNATE);
+				state = 0;
+			}
+			count = 0;
+		}
+	}
+	if (LL_TIM_IsActiveFlag_CC1(TIM3) == SET)
+	{
+		LL_TIM_ClearFlag_CC1(TIM3);
+	}
+	if (LL_TIM_IsActiveFlag_CC2(TIM3) == SET)
+	{
+		LL_TIM_ClearFlag_CC2(TIM3);
 	}
 }
 
@@ -192,8 +344,8 @@ void SevenSeg(uint8_t* num)
 	{
 		LL_GPIO_ResetOutputPin(GPIOC, LL_GPIO_PIN_0 | LL_GPIO_PIN_1 | LL_GPIO_PIN_2 | LL_GPIO_PIN_3);
 		LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_2 | LL_GPIO_PIN_10 | LL_GPIO_PIN_11 | LL_GPIO_PIN_12 | LL_GPIO_PIN_13 | LL_GPIO_PIN_14 | LL_GPIO_PIN_15 | LL_GPIO_PIN_2);
-		
-		if(position == 1)
+
+		if (position == 1)
 		{
 			LL_GPIO_SetOutputPin(GPIOB, LL_GPIO_PIN_3); // dp
 		}
@@ -201,11 +353,11 @@ void SevenSeg(uint8_t* num)
 		{
 			LL_GPIO_ResetOutputPin(GPIOB, LL_GPIO_PIN_3); // dp
 		}
-		
-		switch (*(num + (i-1)))
+
+		switch (*(num + (i - 1)))
 		{
 		case 0:
-			if(i == 1)
+			if (i == 1)
 			{
 				LL_GPIO_ResetOutputPin(GPIOC, digit[position]);
 			}
